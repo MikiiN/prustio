1dac657a6582ae2bb9307c1e1006465380241bf9
Michal Žatečka <zateckamichal8@gmail.com>
2025-10-25 15:51:59 +0200

    tool init commit

--- cli.py
+++ cli.py
@@ -1,14 +1,18 @@
-import typer
+import click
+from importlib.metadata import version, PackageNotFoundError
 
-from pathlib import Path
+PACKAGE_NAME = "prustio"
 
-app = typer.Typer()
+try:
+    __version__ = version(PACKAGE_NAME)
+except PackageNotFoundError:
+    __version__ = "0.0.0+dev"
+
+@click.group()
+@click.version_option(__version__, prog_name=PACKAGE_NAME)
+def cli():
+    pass
 
 
-
-def main():
-    app()
-    
-
-if __name__ == "__main__":
-    app()
+if __name__ == '__main__':
+    cli()
